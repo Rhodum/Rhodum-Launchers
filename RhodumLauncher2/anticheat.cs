f219ac0d79ae5e740acf3c76fc9d52b03159b7b3
TsunamiWind <54415744+TsunamiWind@users.noreply.github.com>
2022-01-15 15:34:07 +0100

    Add files via upload

---
+++ RhodumLauncher2/anticheat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+
+namespace RhodumLauncher2
+{
+    class anticheat
+    {
+        public static void startanticheat()
+        {
+            WebClient wClient = new WebClient();
+            string[] definedPrograms = { "injector", "cheat", "hack", "master131", "guardian", "patcher", "xenos", "ploit", "venom", "exploit", "Vaccine", "fiddler", "Gadget", "Big Bang", "Neferty", "GIFDO", "Traitc0re", "Shad0w" };
+            foreach (string definedProgram in definedPrograms)
+            {
+                System.Diagnostics.Process.GetProcesses()
+                .Where(x => x.ProcessName.StartsWith(definedProgram, StringComparison.OrdinalIgnoreCase))
+                .ToList()
+                .ForEach(x => wClient.DownloadString("http://labs.rhodum.xyz/launcher/report.php?report=" + x.ProcessName));
+            }
+        }
+    }
+}
---
+++ RhodumLauncher2/discord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DiscordRPC;
+
+namespace RhodumLauncher2
+{
+    class discord
+    {
+        public static void launchrpc(string gamename)
+        {
+            DiscordRpcClient client = new DiscordRpcClient("847924026176831518");
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+            {
+                Details = "Rhodum - Playing",
+                State = gamename,
+                Timestamps = Timestamps.Now,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "untitled",
+                    LargeImageText = "Rhodum"
+                }
+            });
+        }
+    }
+}
